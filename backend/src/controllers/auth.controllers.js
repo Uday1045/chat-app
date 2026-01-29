464ec5a4c0606ef009589effe6606c60ff97af30
Uday1045 <tomaruday0412@gmail.com>
2026-01-29 14:48:54 +0530

    refresh

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -112,13 +112,19 @@ export const updateProfile = async (req, res) => {
     }
 };
 export const checkAuth = (req, res) => {
-    try {
-        res.status(200).json(req.user);
-    } catch (error) {
-        console.log("Error in checkAuth controller", error.message);
-        res.status(500).json({ message: "Internal Server Error" });
+  try {
+    if (!req.user) {
+      return res.status(401).json({ message: "Not authenticated" });
     }
+
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
+
+
 export const getUsersByLocation = async (req, res) => {
   try {
     const { location } = req.params;
