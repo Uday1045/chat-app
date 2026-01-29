4b93fdf03e01776dff441e132dcfd4988e3b2358
Uday1045 <tomaruday0412@gmail.com>
2026-01-29 18:02:10 +0530

    Changed ENV

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,15 +1,17 @@
 import jwt from"jsonwebtoken"
 
 
-export const generateToken=(userId,res) =>{
-    const token=jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:"7d",
-    });
-    res.cookie("jwt",token,{
-       httpOnly: true,
-  secure: true, // true on Render
-  sameSite: "none", 
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
-return token;
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  return token;
 };
